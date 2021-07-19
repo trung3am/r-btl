6dcd8d1459ca8834395b530459728e6aba6ddb26
Trung <tqtrung1209@gmail.com>
2021-07-19 12:19:05 +0700

    uppdate phan IV

--- Phan IV.R
+++ Phan IV.R
@@ -180,8 +180,12 @@ LOR.GK11 <- as.data.frame(bind_rows(DiemGK.1921.LO11,DiemGK.1922.LO11,DiemGK.192
 LOR.GK11[is.na(LOR.GK11)] <- 0
 #Sum column
 LOR.GK11 <- as.data.frame(apply(LOR.GK11,2,sum))
-
 LOR.GK11$SoCauTLDung <- row.names(LOR.GK11)
+#deselect 0 right anwser row
+LOR.GK11<- LOR.GK11[LOR.GK11$SoCauTLDung > 0,] 
+#re order data
+LOR.GK11<- transform(LOR.GK11, SoCauTLDung = as.numeric(SoCauTLDung))
+LOR.GK11<- arrange(LOR.GK11,SoCauTLDung)
 LOR.GK11
 
 #Learning outcome 12 (merge into usable data frame)
@@ -190,9 +194,8 @@ LOR.GK12 <- as.data.frame(bind_rows(DiemGK.1921.LO12,DiemGK.1922.LO12,DiemGK.192
 LOR.GK12[is.na(LOR.GK12)] <- 0
 #Sum column
 LOR.GK12 <- as.data.frame(apply(LOR.GK12,2,sum))
-
 LOR.GK12$SoCauTLDung <- row.names(LOR.GK12)
-LOR.GK12
+
 
 #Learning outcome 21 (merge into usable data frame)
 LOR.GK21 <- as.data.frame(bind_rows(DiemGK.1921.LO21,DiemGK.1922.LO21,DiemGK.1923.LO21,DiemGK.1924.LO21))
@@ -202,7 +205,9 @@ LOR.GK21[is.na(LOR.GK21)] <- 0
 LOR.GK21 <- as.data.frame(apply(LOR.GK21,2,sum))
 
 LOR.GK21$SoCauTLDung <- row.names(LOR.GK21)
-LOR.GK21
+#deselect 0 right anwser row
+LOR.GK21<- LOR.GK21[LOR.GK21$SoCauTLDung > 0,] 
+
 
 #Learning outcome 22 (merge into usable data frame)
 LOR.GK22 <- as.data.frame(bind_rows(DiemGK.1921.LO22,DiemGK.1922.LO22,DiemGK.1923.LO22,DiemGK.1924.LO22))
@@ -212,7 +217,9 @@ LOR.GK22[is.na(LOR.GK22)] <- 0
 LOR.GK22 <- as.data.frame(apply(LOR.GK22,2,sum))
 
 LOR.GK22$SoCauTLDung <- row.names(LOR.GK22)
-LOR.GK22
+#deselect 0 right anwser row
+LOR.GK22<- LOR.GK22[LOR.GK22$SoCauTLDung > 0,] 
+
 
 #Learning outcome 31 (merge into usable data frame)
 LOR.GK31 <- as.data.frame(bind_rows(DiemGK.1921.LO31,DiemGK.1922.LO31,DiemGK.1923.LO31,DiemGK.1924.LO31))
@@ -222,9 +229,12 @@ LOR.GK31[is.na(LOR.GK31)] <- 0
 LOR.GK31 <- as.data.frame(apply(LOR.GK31,2,sum))
 
 LOR.GK31$SoCauTLDung <- row.names(LOR.GK31)
-LOR.GK31
+#deselect 0 right anwser row
+LOR.GK31<- LOR.GK31[LOR.GK31$SoCauTLDung > 0,] 
+
 
 #Graph ---
+
 
 GLOR.GK11 <- ggplot(LOR.GK11, aes(x = as.character(LOR.GK11[,2]), y = LOR.GK11[,1])) + geom_bar(stat = "identity") + geom_text(aes(label=LOR.GK11[,1]), vjust=-0.3, size=3.5, )+
   labs(title = "Learning Outcome 11", x = "So cau tra loi dung", y = "So sinh vien") + theme(panel.background = element_rect(fill = "#d557d5"))
@@ -357,6 +367,10 @@ LOR.CK23 <- as.data.frame(apply(LOR.CK23,2,sum))
 LOR.CK23$SoCauTLDung <- row.names(LOR.CK23)
 LOR.CK23
 
+#deselect 0 right anwser row
+LOR.CK23<- LOR.CK23[LOR.CK23$SoCauTLDung > 0,] 
+
+
 #Learning outcome 31 (merge into usable data frame)
 
 LOR.CK31 <- as.data.frame(bind_rows(DiemCK.1921.LO31,DiemCK.1922.LO31,DiemCK.1923.LO31,DiemCK.1924.LO31))
@@ -397,14 +411,162 @@ GLOR.CK <-ggarrange(GLOR.CK12,GLOR.CK23,GLOR.CK31,GLOR.CK32, ncol = 3, nrow = 2)
 GLOR.CK
 
 #Problem 7 ----
-#Learning outcome mid term and final exam
+#Data of Learning outcome mid term and final exam
 LO.data<- as.data.frame(bind_rows(table(GKCDR.1921),table(CKCDR.1921)))
-#Convert in to data for chart drawing
+#Convert in to usable data for chart drawing
 LO.data[is.na(LO.data)] <- 0
 LO.data<- as.data.frame(apply(LO.data,2,sum))
 LO.data$LO <- row.names(LO.data)
 
+#graph 
 GLO.data <- ggplot(LO.data, aes(x = as.character(LO.data[,2]), y = LO.data[,1])) + geom_bar(stat = "identity") + geom_text(aes(label=LO.data[,1]), vjust=-0.3, size=3.5, )+
   labs(title = "Question per learning outcome", x = "Learning outcome", y = "Questions quantity") + theme(panel.background = element_rect(fill = "#57d5c5"))
 GLO.data
+
+#
+TotalQFrameCK<-as.data.frame(table(as.numeric(CKCDR[1,-1])))
+TotalQFrameGK<-as.data.frame(table(as.numeric(GKCDR[1,-1])))
+
+g <- as.numeric(t[1,2])
+
 #Problem 8 ----
+#convert data from problem 6 for reuse
+
+LOW.GK11 <- LOR.GK11
+LOW.GK11 <- transform(LOW.GK11, SoCauTLDung = as.numeric(SoCauTLDung))
+LOW.GK11$SoCauTLSai <- abs(LOW.GK11$SoCauTLDung - as.numeric(TotalQFrameGK[1,2]))
+LOW.GK11 <- LOW.GK11[,c(1,3)]
+#deselect 0
+LOW.GK11 <- LOW.GK11[LOW.GK11$SoCauTLSai > 0,]
+#percentaging
+LOW.GK11[,1] <- round(LOW.GK11[,1]/sum(LOW.GK11[,1]),3)
+LOW.GK11
+
+LOW.GK12 <- LOR.GK12
+LOW.GK12 <- transform(LOW.GK12, SoCauTLDung = as.numeric(SoCauTLDung))
+LOW.GK12$SoCauTLSai <- abs(LOW.GK12$SoCauTLDung - as.numeric(TotalQFrameGK[2,2]))
+LOW.GK12 <- LOW.GK12[,c(1,3)]
+#deselect 0
+LOW.GK12 <- LOW.GK12[LOW.GK12$SoCauTLSai > 0,]
+#percentaging
+LOW.GK12[,1] <- round(LOW.GK12[,1]/sum(LOW.GK12[,1]),3)
+LOW.GK12
+
+LOW.GK21 <- LOR.GK21
+LOW.GK21 <- transform(LOW.GK21, SoCauTLDung = as.numeric(SoCauTLDung))
+LOW.GK21$SoCauTLSai <- abs(LOW.GK21$SoCauTLDung - as.numeric(TotalQFrameGK[3,2]))
+#deselect 0
+LOW.GK21 <- LOW.GK21[LOW.GK21$SoCauTLSai > 0,]
+LOW.GK21
+#percentaging
+LOW.GK21[,1] <- round(LOW.GK21[,1]/sum(LOW.GK21[,1]),3)
+LOW.GK21
+
+
+LOW.GK22 <- LOR.GK22
+LOW.GK22 <- transform(LOW.GK22, SoCauTLDung = as.numeric(SoCauTLDung))
+LOW.GK22$SoCauTLSai <- abs(LOW.GK22$SoCauTLDung - as.numeric(TotalQFrameGK[4,2]))
+LOW.GK22 <- LOW.GK22[,c(1,3)]
+#deselect 0
+LOW.GK22 <- LOW.GK22[LOW.GK22$SoCauTLSai > 0,]
+#percentaging
+LOW.GK22[,1] <- round(LOW.GK22[,1]/sum(LOW.GK22[,1]),3)
+LOW.GK22
+
+LOW.GK31 <- LOR.GK31
+LOW.GK31 <- transform(LOW.GK31, SoCauTLDung = as.numeric(SoCauTLDung))
+LOW.GK31$SoCauTLSai <- abs(LOW.GK31$SoCauTLDung - as.numeric(TotalQFrameGK[5,2]))
+LOW.GK31 <- LOW.GK31[,c(1,3)]
+#deselect 0
+LOW.GK31 <- LOW.GK31[LOW.GK31$SoCauTLSai > 0,]
+#percentaging
+LOW.GK31[,1] <- round(LOW.GK31[,1]/sum(LOW.GK31[,1]),3)
+LOW.GK31
+
+#Graph ---
+
+GLOW.GK11 <- ggplot(LOW.GK11, aes(x = as.character(LOW.GK11[,2]), y = LOW.GK11[,1])) + geom_bar(stat = "identity") + geom_text(aes(label=LOW.GK11[,1]), vjust=-0.3, size=3.5, )+
+  labs(title = "Learning Outcome 11", x = "So cau tra loi sai", y = "tan suat tuong duong") + theme(panel.background = element_rect(fill = "#d557d5"))
+
+GLOW.GK12 <-  ggplot(LOW.GK12, aes(x = as.character(LOW.GK12[,2]), y = LOW.GK12[,1])) + geom_bar(stat = "identity") + geom_text(aes(label=LOW.GK12[,1]), vjust=-0.3, size=3.5, )+
+  labs(title = "Learning Outcome 12", x = "So cau tra loi sai", y = "tan suat tuong duong") + theme(panel.background = element_rect(fill = "#d557d5"))
+
+GLOW.GK21 <- ggplot(LOW.GK21, aes(x = as.character(LOW.GK21[,2]), y = LOW.GK21[,1])) + geom_bar(stat = "identity") + geom_text(aes(label=LOW.GK21[,1]), vjust=-0.3, size=3.5, )+
+  labs(title = "Learning Outcome 21", x = "So cau tra loi sai", y = "tan suat tuong duong") + theme(panel.background = element_rect(fill = "#d557d5"))
+
+GLOW.GK22 <-  ggplot(LOW.GK22, aes(x = as.character(LOW.GK22[,2]), y = LOW.GK22[,1])) + geom_bar(stat = "identity") + geom_text(aes(label=LOW.GK22[,1]), vjust=-0.3, size=3.5, )+
+  labs(title = "Learning Outcome 22", x = "So cau tra loi sai", y = "tan suat tuong duong") + theme(panel.background = element_rect(fill = "#d557d5"))
+
+GLOW.GK31 <- ggplot(LOW.GK31, aes(x = as.character(LOW.GK31[,2]), y = LOW.GK31[,1])) + geom_bar(stat = "identity") + geom_text(aes(label=LOW.GK31[,1]), vjust=-0.3, size=3.5, )+
+  labs(title = "Learning Outcome 31", x = "So cau tra loi sai", y = "tan suat tuong duong") + theme(panel.background = element_rect(fill = "#d557d5"))
+
+GLOW.GK <-ggarrange(GLOW.GK11,GLOW.GK12,GLOW.GK21,GLOW.GK22,GLOW.GK31, ncol = 3, nrow = 2)
+
+GLOW.GK
+
+#Problem 9
+
+
+names(table(CKCDR.1921))
+
+#convert data from problem 6 for reuse
+
+LOW.CK12 <- LOR.CK12
+LOW.CK12 <- transform(LOW.CK12, SoCauTLDung = as.numeric(SoCauTLDung))
+LOW.CK12$SoCauTLSai <- abs(LOW.CK12$SoCauTLDung - as.numeric(TotalQFrameCK[1,2]))
+LOW.CK12 <- LOW.CK12[,c(1,3)]
+#deselect 0
+LOW.CK12 <- LOW.CK12[LOW.CK12$SoCauTLSai > 0,]
+#percentaging
+LOW.CK12[,1] <- round(LOW.CK12[,1]/sum(LOW.CK12[,1]),3)
+LOW.CK12
+
+LOW.CK23 <- LOR.CK23
+LOW.CK23 <- transform(LOW.CK23, SoCauTLDung = as.numeric(SoCauTLDung))
+LOW.CK23$SoCauTLSai <- abs(LOW.CK23$SoCauTLDung - as.numeric(TotalQFrameCK[2,2]))
+LOW.CK23 <- LOW.CK23[,c(1,3)]
+#deselect 0
+LOW.CK23 <- LOW.CK23[LOW.CK23$SoCauTLSai > 0,]
+#percentaging
+LOW.CK23[,1] <- round(LOW.CK23[,1]/sum(LOW.CK23[,1]),3)
+LOW.CK23
+
+LOW.CK31 <- LOR.CK31
+LOW.CK31 <- transform(LOW.CK31, SoCauTLDung = as.numeric(SoCauTLDung))
+LOW.CK31$SoCauTLSai <- abs(LOW.CK31$SoCauTLDung - as.numeric(TotalQFrameCK[3,2]))
+LOW.CK31 <- LOW.CK31[,c(1,3)]
+#deselect 0
+LOW.CK31 <- LOW.CK31[LOW.CK31$SoCauTLSai > 0,]
+#percentaging
+LOW.CK31[,1] <- round(LOW.CK31[,1]/sum(LOW.CK31[,1]),3)
+LOW.CK31
+
+LOW.CK32 <- LOR.CK32
+LOW.CK32 <- transform(LOW.CK32, SoCauTLDung = as.numeric(SoCauTLDung))
+LOW.CK32$SoCauTLSai <- abs(LOW.CK32$SoCauTLDung - as.numeric(TotalQFrameCK[4,2]))
+LOW.CK32 <- LOW.CK32[,c(1,3)]
+#deselect 0
+LOW.CK32 <- LOW.CK32[LOW.CK32$SoCauTLSai > 0,]
+#percentaging
+LOW.CK32[,1] <- round(LOW.CK32[,1]/sum(LOW.CK32[,1]),3)
+LOW.CK32
+
+#Graph---
+
+GLOW.CK12 <- ggplot(LOW.CK12, aes(x = as.character(LOW.CK12[,2]), y = LOW.CK12[,1])) + geom_bar(stat = "identity") + geom_text(aes(label=LOW.CK12[,1]), vjust=-0.3, size=3.5, )+
+  labs(title = "Learning Outcome 12", x = "So cau tra loi sai", y = "tan suat tuong duong") + theme(panel.background = element_rect(fill = "#d557d5"))
+
+GLOW.CK23 <- ggplot(LOW.CK23, aes(x = as.character(LOW.CK23[,2]), y = LOW.CK23[,1])) + geom_bar(stat = "identity") + geom_text(aes(label=LOW.CK23[,1]), vjust=-0.3, size=3.5, )+
+  labs(title = "Learning Outcome 23", x = "So cau tra loi sai", y = "tan suat tuong duong") + theme(panel.background = element_rect(fill = "#d557d5"))
+
+GLOW.CK31 <- ggplot(LOW.CK31, aes(x = as.character(LOW.CK31[,2]), y = LOW.CK31[,1])) + geom_bar(stat = "identity") + geom_text(aes(label=LOW.CK31[,1]), vjust=-0.3, size=3.5, )+
+  labs(title = "Learning Outcome 31", x = "So cau tra loi sai", y = "tan suat tuong duong") + theme(panel.background = element_rect(fill = "#d557d5"))
+
+GLOW.CK32 <- ggplot(LOW.CK32, aes(x = as.character(LOW.CK32[,2]), y = LOW.CK32[,1])) + geom_bar(stat = "identity") + geom_text(aes(label=LOW.CK32[,1]), vjust=-0.3, size=3.5, )+
+  labs(title = "Learning Outcome 32", x = "So cau tra loi sai", y = "tan suat tuong duong") + theme(panel.background = element_rect(fill = "#d557d5"))
+
+GLOW.CK <-ggarrange(GLOW.CK12,GLOW.CK23,GLOW.CK31,GLOW.CK32, ncol = 3, nrow = 2)
+
+GLOW.CK
+
+#Problem 10 ---
